9dd87db4256cb50602f53a829afd2e51186a9171
Ruthenium74 <Ruthenium74@yandex.ru>
2020-06-24 21:16:33 +0500

    Fix assertMatch for Iterable

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -50,6 +50,6 @@ public class MealTestData {
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        assertThat(actual).usingFieldByFieldElementComparator().isEqualTo(expected);
     }
 }

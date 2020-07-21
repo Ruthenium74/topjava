5c2b6c78650a416843166ae665853fe8e55a1952
Ruthenium74 <Ruthenium74@yandex.ru>
2020-07-21 21:50:48 +0500

    Add fake User reference to Meal in JdbcMealRepository.save

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.validation.*;
@@ -46,6 +47,7 @@ public class JdbcMealRepository implements MealRepository {
     @Override
     @Transactional
     public Meal save(Meal meal, int userId) {
+        meal.setUser(new User());
         Set<ConstraintViolation<Meal>> constraintViolations = validator.validate(meal);
         if (!constraintViolations.isEmpty()) {
             throw new ConstraintViolationException(constraintViolations);
